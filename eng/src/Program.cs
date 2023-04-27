6227bc9d5bb98fdbacd55a1d9e6988dd046b882d
Antonín Procházka <antonin@postsharp.net>
2023-04-27 09:01:32 +0200

    Release 2023.1 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
-using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
-using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Linq;
 
 var product = new Product( Dependencies.MetalamaLinqPad )
 {
@@ -18,10 +15,12 @@ var product = new Product( Dependencies.MetalamaLinqPad )
     PublicArtifacts = Pattern.Create( "Metalama.LinqPad.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
     MainVersionDependency = Dependencies.Metalama,
-    Configurations = Product.DefaultConfigurations
-        .WithValue(
-        BuildConfiguration.Public, Product.DefaultConfigurations.Public with { 
-            PublicPublishers = Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() } )
+
+    // MergePublisher disabled for 2023.1.
+    // Configurations = Product.DefaultConfigurations
+    //     .WithValue(
+    //     BuildConfiguration.Public, Product.DefaultConfigurations.Public with { 
+    //         PublicPublishers = Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() } )
 };
 
 var commandApp = new CommandApp();
