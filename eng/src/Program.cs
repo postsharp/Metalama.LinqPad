242504775f5fff172948238b663fb4973efc56d6
Antonín Procházka <antonin@postsharp.net>
2023-06-12 17:00:34 +0200

    Release 2023.2 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -3,24 +3,19 @@
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
-using PostSharp.Engineering.BuildTools.Dependencies.Model;
+using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_2;
 
-var product = new Product( Dependencies.MetalamaLinqPad )
+var product = new Product( MetalamaDependencies.MetalamaLinqPad )
 {
     Solutions = new Solution[]
     {
         new DotNetSolution( "Metalama.LinqPad.sln" ) { CanFormatCode = true }
     },
     PublicArtifacts = Pattern.Create( "Metalama.LinqPad.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
-    MainVersionDependency = Dependencies.Metalama,
-
-    // MergePublisher disabled for 2023.1.
-    // Configurations = Product.DefaultConfigurations
-    //     .WithValue(
-    //     BuildConfiguration.Public, Product.DefaultConfigurations.Public with { 
-    //         PublicPublishers = Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() } )
+    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering, MetalamaDependencies.Metalama },
+    MainVersionDependency = MetalamaDependencies.Metalama
 };
 
 var commandApp = new CommandApp();
