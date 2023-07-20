8f203f8b51c3a720c971926e86249499297e0339
Antonín Procházka <antonin@postsharp.net>
2023-07-20 18:59:31 +0200

    Release 2023.3 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -18,7 +18,7 @@ using System.Management;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_2;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_3;
 
 var product = new Product( MetalamaDependencies.MetalamaLinqPad )
 {
